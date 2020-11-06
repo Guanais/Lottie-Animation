89b3dcc3e8cf2c4ee1dbdf9323b3a9ae76e99cbc
Kaio <kaioaguanais@gmail.com>
2020-11-05 16:32:23 -0800

    Initial Commit

---
+++ Lottie Animation/Lottie_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lottie_AnimationApp.swift
+//  Lottie Animation
+//
+//  Created by Kaio Guanais on 2020-11-05.
+//
+
+import SwiftUI
+
+@main
+struct Lottie_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
